8d7af0b5e17c6667881431a6fa9607f0add652e5
Sreevalli <sreevalli.popuri@gmail.com>
2025-06-25 23:34:23 +0530

    Implement from validation with inline error messages for empty fields.

--- src/XDisplayName.js
+++ src/XDisplayName.js
@@ -5,17 +5,22 @@ function XDisplayName(){
     const [firstName, setFirstName] = useState(""); // stores first name input
     const [lastName, setlastName] = useState(""); // stores last name input
     const [fullName, setFullName] = useState(""); // stores full name 
+    const [error, setError] = useState(false); // shows error
   
 
     // Handle form submission
 const handleSubmit = (e) => {
     e.preventDefault(); // prevent page reload.
+    console.log("form submission prevented")
 
     // checks if both fields are filled.
     if(!firstName || !lastName){
-
+        setError(true); // show error 
         return;
     }
+
+    setError(false); // clear error on valid submission
+
 
     // set and display full name
     setFullName(`${firstName} ${lastName}`);
@@ -40,8 +45,14 @@ const handleSubmit = (e) => {
         <input type = "text"
         value = {firstName}
         onChange = {(e) => setFirstName(e.target.value)} //update state
-        required  //triggers browser validation
+        name="firstName"
         />
+
+         {/* Show error if first name is empty */}
+          {error && !firstName && (
+            <p>Please fill out this field.</p>
+          )}
+        
         </div>
 
         <div> 
@@ -51,9 +62,13 @@ const handleSubmit = (e) => {
             <input type = "text" 
              value = {lastName}
              onChange = {(e) => setlastName(e.target.value)} // update state
-            required  // triggers browser validation
+            name="lastName"
 
             />
+
+            {/* Show error if last name is empty */}
+            {error && !lastName && (<p> Please fill out this field.</p>)}
+
         </div>
     
         <button type="submit">Submit</button>
